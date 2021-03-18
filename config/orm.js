c6560cd2488f38ac8324ca81be1f0309539ce12f
Justin Hohing <jnh0627@yahoo.com>
2021-03-17 23:00:17 -0400

    Edited orm methods and added first api call to controller file

--- config/orm.js
+++ config/orm.js
@@ -1,37 +1,40 @@
-const connection = require("./connection.js");
+var connection = require("./connection.js");
 
-const orm = {
-    selectAll: function() {
-    //   var queryString = "SELECT * FROM ?? WHERE ?? = ?";
-    //   // SELECT * FROM pets WHERE animal_name = "Rachel"
-    //   connection.query(queryString, [tableInput, colToSearch, valOfCol], function(err, result) {
-    //     if (err) throw err;
-    //     console.log(result);
-    //   });
+var orm = {
+    selectAll: (table, cb) => {
+        var queryString = "SELECT * FROM " + table + ";";
+        connection.query(queryString, (err, result) => {
+            if (err) throw err;
+            cb(result);
+        });
     },
-    insertOne: function() {
-    //   var queryString = "SELECT ?? FROM ?? ORDER BY ?? DESC";
-    //   // SELECT animal_name FROM pets ORDER BY price DESC;
-    //   console.log(queryString);
-    //   connection.query(queryString, [whatToSelect, table, orderCol], function(err, result) {
-    //     if (err) throw err;
-    //     console.log(result);
-    //   });
+    insertOne: (table, column, value, cb) => {
+        var queryString = "INSERT INTO " + table;
+
+        queryString += "(";
+        queryString += column.toString();
+        queryString += ") ";
+        queryString += "VALUES (";
+        queryString += printQuestionMarks(value.length);
+        queryString += ")";
+
+        console.log(queryString);
+
+        connection.query(queryString, value, (err, result) => {
+            if (err) throw err;
+            cb(result);
+        });
     },
-    updateOne: function() {
-    //   var queryString =
-    //     "SELECT ??, COUNT(??) AS count FROM ?? LEFT JOIN ?? ON ??.??= ??.id GROUP BY ?? ORDER BY count DESC LIMIT 1";
-    //   // SELECT buyer_name, COUNT(buyer_name) AS count FROM buyers LEFT JOIN pets ON pets.buyer_id= buyers.id GROUP BY buyer_name ORDER BY count DESC LIMIT 1"
-  
-    //   connection.query(
-    //     queryString,
-    //     [tableOneCol, tableOneCol, tableOne, tableTwo, tableTwo, tableTwoForeignKey, tableOne, tableOneCol],
-    //     function(err, result) {
-    //       if (err) throw err;
-    //       console.log(result);
-    //     }
-    //   );
+    updateOne: (table, column, burgerId, cb) => {
+        var queryString = "UPDATE ?? SET ?? = 1 WHERE id = ?" ;
+        console.log(queryString);
+
+        connection.query(queryString, [table, column, burgerId], (err, result) => {
+            if (err) throw err;
+
+            cb(result);
+        });
     }
-  };
-  
-  module.exports = orm;
+};
+
+module.exports = orm;
--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -5,5 +5,16 @@ const router = express.Router();
 //Import the model (burger.js) to use the db functions
 const burger = require("../models/burger.js");
 
+router.get("/", (req, res) => {
+    console.log("BURGER");
+    burger.selectAll(data => {
+        var hbsobject = {
+            burger: data
+        };
+        console.log(hbsobject);
+        res.render("index", hbsobject);
+    });
+});
+
 // Export routes
 module.exports = router;
