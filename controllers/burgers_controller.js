fc087dcc1914b47ae2535449e988af97f728c942
Justin Hohing <jnh0627@yahoo.com>
2021-03-17 23:08:45 -0400

    Added post and put api routes to controller file

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -5,6 +5,8 @@ const router = express.Router();
 //Import the model (burger.js) to use the db functions
 const burger = require("../models/burger.js");
 
+//Routes
+
 router.get("/", (req, res) => {
     console.log("BURGER");
     burger.selectAll(data => {
@@ -13,6 +15,26 @@ router.get("/", (req, res) => {
         };
         console.log(hbsobject);
         res.render("index", hbsobject);
+    });
+});
+
+router.post("/api/burger", (req, res) => {
+    burger.insertOne("burger_name", req.body.burger_name, 
+    function (result) {
+        res.json({id: result.insertId})
+    });
+});
+
+router.put("/api/burger/:id", (req, res) => {
+    const burgerId = req.params.id;
+
+    burger.updateOne(burgerId, function(result) {
+        if(result.changedRows == 0){
+            return res.status(404).end();
+        }
+        else {
+            res.status(200).end();
+        }        
     });
 });
 
